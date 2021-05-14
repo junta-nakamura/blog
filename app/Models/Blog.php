e3c36e4d4328f06e7612a870dd4da4d3f236654e
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-05-14 21:54:00 +0900

    モデル作成

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    use HasFactory;
+    // テーブル名
+    protected $table = "blogs";
+
+    // 可変項目
+    protected $fillable =
+    [
+        'title',
+        'contents'
+    ];
 }
